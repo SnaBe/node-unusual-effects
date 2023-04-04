56d3c01e3fd85369b1083b2a72984b7dd37fe161
SnaBe <fynboen1999@gmail.com>
2023-04-05 00:40:27 +0200

    Add unit test for the getEffectFromObject function

--- test/test.js
+++ test/test.js
@@ -1,20 +1,27 @@
-// We'll use Mocha and Chai for testing the module's methods
+// Require Mocha and Chai for unit testing the functions
 const expect = require('chai').expect
+
+// Required for interacting with the Steamcommunity API
+const SteamCommunity = require('steamcommunity')
 
 // Require the module itself
 // Replace this with const Unusual = require('unusual-effects'); if used outside of the module directory
-const { findEffectByName, findEffectById, getEffectImages } = require('../index')
+const { findEffectByName, findEffectById, getEffectImages, getEffectFromObject } = require('../index')
 
-// Our unit tests
+// Steamcommunity instance for getting user inventories
+const community = new SteamCommunity()
+
+// The unit tests for each function
 describe('unusual-effects tests', () => {
     // Test and verify the return value of findEffectByName()
     describe('findEffectByName test', () => {
-        // Return type should be a numbrt
+        // Return type should be a number
         it('should return 17', () => {
             // Get the effect id for Sunbeams
             const result = findEffectByName('Sunbeams')
 
             // The result should equal 17 (The effect id of Sunbeams)
+            expect(result).to.be.a('number')
             expect(result).to.equal(17)
         })
     })
@@ -27,6 +34,7 @@ describe('unusual-effects tests', () => {
             const result = findEffectById(14)
 
             // The result should equal Scorching Flames (The effect name matching id 14)
+            expect(result).to.be.a('string')
             expect(result).to.equal('Scorching Flames')
         })
     })
@@ -49,20 +57,53 @@ describe('unusual-effects tests', () => {
 
             // The result should equal an array, as Showstopper has more than one variant (RED or BLU)
             expect(result).to.be.an('array')
+            expect(result).to.have.length(2)
         })
     })
 
     // Test and verify the return value of getEffectImages()
     describe('getEffectImages tests', () => {
-        // Return type should be an object
+        // Return type should be an GetEffectImagesResponse object
         it('should return an object', () => {
             // Get the effect images(s) for Bonzo The All-Gnawing
             const result = getEffectImages('Bonzo The All-Gnawing')
 
             // The result should equal an object
+            // and must have a property named "images" 
             expect(result).to.be.an('object')
             expect(result).to.have.property('images')
             expect(result).to.have.property('name', 'Bonzo The All-Gnawing')
+        })
+    })
+
+    // Test and verify the return value of getEffectFromObject()
+    describe('getEffectFromObject tests', () => {
+        // Return type should be an GetEffectFromObjectResponse object
+        it('should return an Unusual object', (done) => {
+            // A "random" Steam user's unique id
+            const user = '76561197977733292'
+
+            // Get that user's Team Fortress 2 inventory
+            community.getUserInventoryContents(user, 440, 2, true, (err, inventory) => {
+                // Error getting currency data
+                if (err) return done(err)
+
+                // Filter the inventory for items with "Unusual" in their name
+                const unusuals = inventory.filter((item => item.market_hash_name.includes('Unusual')))
+
+                // Get the Unusual Effect from the first EconItem object in the array
+                const result = getEffectFromObject(unusuals[0])
+
+                // The Steam response should have status code 200 (ok),
+                // it should also be an object
+                // and must have a property named "images" 
+                expect(result).to.be.an('object')
+                expect(result).to.have.property('images')
+                expect(result.images).to.have.property('small')
+
+                // Call done to end the test when the callback is invoked
+                done()
+            })
         })
     })
 })
